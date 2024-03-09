ed0a7998594ba09e4cb04fbf21d038cf9075c1ce
MaddoJP <marko@ansinger.de>
2024-03-09 21:25:03 +0100

    add ability to delete file

--- convex/files.ts
+++ convex/files.ts
@@ -78,3 +78,34 @@ export const getFiles = query({
       .collect();
   },
 });
+
+export const deleteFile = mutation({
+    args: {
+      fileId: v.id("files"),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+          throw new ConvexError("you do not have access to this org");
+        }
+
+        const file = await ctx.db.get(args.fileId)
+
+        if (!file) {
+            throw new ConvexError("This file doesn't exist");
+        }
+    
+        const hasAccess = await hasAccessToOrg(
+          ctx,
+          identity.tokenIdentifier,
+          file.orgId
+        );
+    
+        if (!hasAccess) {
+            throw new ConvexError("You don't have access to this file");
+        }
+
+        await ctx.db.delete(args.fileId)
+    },
+  });
